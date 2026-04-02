9dc70a4e2097c3127e30f35c39967805e0d17fb9
Felipe Sakuma <fycorsak@gmail.com>
2026-04-02 20:07:40 -0300

    add spring

---
+++ backend/src/main/java/com/comp/reparo/ReparoApplication.java
@@ -0,0 +1,13 @@
+package com.comp.reparo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReparoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReparoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/comp/reparo/ReparoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp.reparo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReparoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
