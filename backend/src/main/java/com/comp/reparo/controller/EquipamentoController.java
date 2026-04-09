0828fdbe96f03660c2bf76c7a3add1048cd1043b
Felipe Sakuma <fycorsak@gmail.com>
2026-04-09 20:17:02 -0300

    add put and delete endpoints for all routes and standardize return with ResponseEntity

--- backend/src/main/java/com/comp/reparo/controller/EquipamentoController.java
+++ backend/src/main/java/com/comp/reparo/controller/EquipamentoController.java
@@ -2,11 +2,10 @@ package com.comp.reparo.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.comp.reparo.model.Equipamento;
 import com.comp.reparo.repository.EquipamentoRepository;
@@ -15,19 +14,36 @@ import com.comp.reparo.repository.EquipamentoRepository;
 @RequestMapping("/equipamentos")
 public class EquipamentoController {
 
-    private final EquipamentoRepository repository;
+    @Autowired
+    private EquipamentoRepository repository;
 
-    public EquipamentoController(EquipamentoRepository repository) {
-        this.repository = repository;
+    @GetMapping(value = "/", produces = "application/json")
+    public ResponseEntity<List<Equipamento>> findAll() {
+        List<Equipamento> list = repository.findAll();
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
-    @GetMapping
-    public List<Equipamento> findAll() {
-        return repository.findAll();
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public ResponseEntity<Equipamento> findById(@PathVariable Long id) {
+        Equipamento equipamento = repository.findById(id).orElseThrow();
+        return new ResponseEntity<>(equipamento, HttpStatus.OK);
     }
 
-    @PostMapping
-    public Equipamento create(@RequestBody Equipamento e) {
-        return repository.save(e);
+    @PostMapping(value = "/", produces = "application/json")
+    public ResponseEntity<Equipamento> create(@RequestBody Equipamento equipamento) {
+        Equipamento created = repository.save(equipamento);
+        return new ResponseEntity<>(created, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/", produces = "application/json")
+    public ResponseEntity<Equipamento> update(@RequestBody Equipamento equipamento) {
+        Equipamento updated = repository.save(equipamento);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}", produces = "application/text")
+    public String delete(@PathVariable("id") Long id) {
+        repository.deleteById(id);
+        return "ok";
     }
 }
--- backend/src/main/java/com/comp/reparo/controller/TecnicoController.java
+++ backend/src/main/java/com/comp/reparo/controller/TecnicoController.java
@@ -2,11 +2,10 @@ package com.comp.reparo.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.comp.reparo.model.Tecnico;
 import com.comp.reparo.repository.TecnicoRepository;
@@ -15,19 +14,36 @@ import com.comp.reparo.repository.TecnicoRepository;
 @RequestMapping("/tecnicos")
 public class TecnicoController {
 
-    private final TecnicoRepository repository;
+    @Autowired
+    private TecnicoRepository repository;
 
-    public TecnicoController(TecnicoRepository repository) {
-        this.repository = repository;
+    @GetMapping(value = "/", produces = "application/json")
+    public ResponseEntity<List<Tecnico>> findAll() {
+        List<Tecnico> list = repository.findAll();
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
-    @GetMapping
-    public List<Tecnico> findAll() {
-        return repository.findAll();
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public ResponseEntity<Tecnico> findById(@PathVariable Long id) {
+        Tecnico tecnico = repository.findById(id).orElseThrow();
+        return new ResponseEntity<>(tecnico, HttpStatus.OK);
     }
 
-    @PostMapping
-    public Tecnico create(@RequestBody Tecnico t) {
-        return repository.save(t);
+    @PostMapping(value = "/", produces = "application/json")
+    public ResponseEntity<Tecnico> create(@RequestBody Tecnico tecnico) {
+        Tecnico created = repository.save(tecnico);
+        return new ResponseEntity<>(created, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/", produces = "application/json")
+    public ResponseEntity<Tecnico> update(@RequestBody Tecnico tecnico) {
+        Tecnico updated = repository.save(tecnico);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}", produces = "application/text")
+    public String delete(@PathVariable("id") Long id) {
+        repository.deleteById(id);
+        return "ok";
     }
 }
